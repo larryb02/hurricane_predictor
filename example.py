ec9ccff21665144dce891e2d673ce4048bcb1adb
Larry Banks <larrykbanks21@gmail.com>
2025-04-29 12:46:11 -0400

    just write output to a file for now

--- example.py
+++ example.py
@@ -44,7 +44,7 @@ io = ZarrBackend()
 # %%
 import earth2studio.run as run
 
-nsteps = 4
+nsteps = 1
 today = datetime.today() - timedelta(days=1)
 date = today.isoformat().split("T")[0]
 io = run.deterministic([date], nsteps, model, data, io)
@@ -60,46 +60,49 @@ print(io.root.tree())
 #
 # Notice that the Zarr IO function has additional APIs to interact with the stored data.
 
+f = open("outputs/model_output.txt", "w")
+f.write(io)
 # %%
-import cartopy
-import cartopy.crs as ccrs
-import matplotlib.pyplot as plt
 
-forecast = f"{date}"
-variable = "t2m"
-step = 4  # lead time = 1 hr
+# import cartopy
+# import cartopy.crs as ccrs
+# import matplotlib.pyplot as plt
 
-plt.close("all")
+# forecast = f"{date}"
+# variable = "t2m"
+# step = 4  # lead time = 1 hr
 
-# Create a correct Lambert Conformal projection
-projection = ccrs.LambertConformal(
-    central_longitude=262.5,
-    central_latitude=38.5,
-    standard_parallels=(38.5, 38.5),
-    globe=ccrs.Globe(semimajor_axis=6371229, semiminor_axis=6371229),
-)
+# plt.close("all")
 
-# Create a figure and axes with the specified projection
-fig, ax = plt.subplots(subplot_kw={"projection": projection}, figsize=(10, 6))
+# # Create a correct Lambert Conformal projection
+# projection = ccrs.LambertConformal(
+#     central_longitude=262.5,
+#     central_latitude=38.5,
+#     standard_parallels=(38.5, 38.5),
+#     globe=ccrs.Globe(semimajor_axis=6371229, semiminor_axis=6371229),
+# )
 
-# Plot the field using pcolormesh
-im = ax.pcolormesh(
-    io["lon"][:],
-    io["lat"][:],
-    io[variable][0, step],
-    transform=ccrs.PlateCarree(),
-    cmap="Spectral_r",
-)
+# # Create a figure and axes with the specified projection
+# fig, ax = plt.subplots(subplot_kw={"projection": projection}, figsize=(10, 6))
 
-# Set state lines
-ax.add_feature(
-    cartopy.feature.STATES.with_scale("50m"), linewidth=0.5, edgecolor="black", zorder=2
-)
+# # Plot the field using pcolormesh
+# im = ax.pcolormesh(
+#     io["lon"][:],
+#     io["lat"][:],
+#     io[variable][0, step],
+#     transform=ccrs.PlateCarree(),
+#     cmap="Spectral_r",
+# )
 
-# Set title
-ax.set_title(f"{forecast} - Lead time: {step}hrs")
+# # Set state lines
+# ax.add_feature(
+#     cartopy.feature.STATES.with_scale("50m"), linewidth=0.5, edgecolor="black", zorder=2
+# )
 
-# Add coastlines and gridlines
-ax.coastlines()
-ax.gridlines()
-plt.savefig(f"outputs/09_{date}_t2m_prediction.jpg")
+# # Set title
+# ax.set_title(f"{forecast} - Lead time: {step}hrs")
+
+# # Add coastlines and gridlines
+# ax.coastlines()
+# ax.gridlines()
+# plt.savefig(f"outputs/09_{date}_t2m_prediction.jpg")
