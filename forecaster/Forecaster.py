85de7b28e1164bd6afc78c69c1782eec359cb3e9
Larry Banks <larrykbanks21@gmail.com>
2025-05-01 17:43:09 -0400

    basic workflow

--- forecaster/Forecaster.py
+++ forecaster/Forecaster.py
@@ -8,28 +8,33 @@ from earth2studio.io import ZarrBackend
 from earth2studio.models.px import StormCast
 import earth2studio.run as run
 
+
 class Forecaster:
     package = StormCast.load_default_package()
     model = StormCast.load_model(package)
-    data = HRRR(max_workers=2,cache=True)
+    data = HRRR(max_workers=2, cache=True)
     conditioning_data_source = GFS_FX()
     model.conditioning_data_source = conditioning_data_source
 
     def __init__(self):
         self.io = ZarrBackend()
-        pass
 
-    """
-    Generates a deterministic output
-    params: 
-        date: Datetime
-        location: lon, lat of input location
-    """
     def forecast(self, date, location):
+        """
+        Generates a deterministic output
+        params:
+            date: Datetime
+            location: lon, lat of input location
+        returns:
+            Dict
+                dict containing model predictions
+        """
         nsteps = 1
         # today = datetime.today() - timedelta(days=1)
         date = date.isoformat().split("T")[0]
-        io = run.deterministic([date], nsteps, Forecaster.model, Forecaster.data, self.io)
+        io = run.deterministic(
+            [date], nsteps, Forecaster.model, Forecaster.data, self.io
+        )
         # now post process
-        
+
         return "computing!"
